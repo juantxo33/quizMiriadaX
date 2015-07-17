8f65f15835925b4d477969cdbe43074e149a87c7
juantxo33 <jjmarkss@gmail.com>
2015-07-18 00:47:20 +0200

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quizController');
+
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/quizes/question', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+router.get('/quizes/answer', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
